9af051e6f88d5f353075089c6b9e539e2031b37e
ImaniiTy <augusto.vieira@dcomp.ufs.br>
2017-04-20 18:22:39 -0300

    Error PopUp

---
+++ src/gui/ErrorPopUp.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+/**
+ *
+ * @author jainec
+ */
+@SuppressWarnings("serial")
+public abstract class ErrorPopUp extends JFrame{
+    protected String mensagem;
+    public ErrorPopUp () {
+        mensagem = getMensagem();
+        setBounds(Principal.screenResolution.width/2 - 150, Principal.screenResolution.height/2 - 80, 300, 160);
+        setLayout(new BorderLayout());
+        JButton okButton = new JButton("OK");
+        okButton.setBackground(Color.DARK_GRAY);
+        okButton.setForeground(Color.WHITE);
+        okButton.setPreferredSize(new Dimension(80, 40));
+        okButton.setBorder(null);
+        okButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				
+			}
+		});
+        TextPanel textPanel = new TextPanel(mensagem);
+        setVisible(true);
+        //Add..
+        add(textPanel, BorderLayout.CENTER); add(okButton, BorderLayout.SOUTH);
+    }
+    
+    public abstract String getMensagem();
+    
+    public class TextPanel extends JPanel {
+    	private String text;
+    	private JLabel textArea;
+    	
+    	public TextPanel(String text) {
+			this.text = text;
+			setBackground(Color.GRAY);
+			textArea = new JLabel(text, SwingConstants.CENTER);
+			textArea.setForeground(Color.WHITE);
+			textArea.setVerticalAlignment(SwingConstants.CENTER);
+			textArea.setPreferredSize(new Dimension(300, 100));
+			add(textArea);
+		}
+
+		
+    }
+   
+}
---
+++ src/gui/OpenErrorPopUp.java
@@ -0,0 +1,12 @@
+package gui;
+
+@SuppressWarnings("serial")
+public class OpenErrorPopUp extends ErrorPopUp {
+
+	@Override
+	public String getMensagem() {
+		// TODO Auto-generated method stub
+		return "Erro ao Abrir o Arquivo";
+	}
+
+}
